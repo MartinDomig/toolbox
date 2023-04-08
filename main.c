e69a3314ca43138ef010611a78121fe08d749327
Martin Domig <martin@domig.net>
2023-04-08 14:43:48 +0200

    glib: command line argument parsing

--- main.c
+++ main.c
@@ -3,6 +3,31 @@
 #include <glib-unix.h>
 
 static GMainLoop *loop = NULL;
+static gchar *server = NULL;
+
+static GOptionEntry entries[] =
+    {
+        {"host", 'h', 0, G_OPTION_ARG_STRING, &server, "Host name to look up", "host"},
+        {NULL}};
+
+static void parse_options(int argc, char **argv)
+{
+    g_autoptr(GOptionContext) context = g_option_context_new("- perform a DNS lookup");
+    g_option_context_add_main_entries(context, entries, NULL);
+
+    g_autoptr(GError) error = NULL;
+    if (!g_option_context_parse(context, &argc, &argv, &error))
+    {
+        g_print("option parsing failed: %s\n", error->message);
+        exit(EXIT_FAILURE);
+    }
+
+    if (!server)
+    {
+        g_print("No host name given. %s", g_option_context_get_help(context, TRUE, NULL));
+        exit(EXIT_FAILURE);
+    }
+}
 
 static gboolean signal_handler(gpointer user_data)
 {
@@ -13,7 +38,7 @@ static gboolean signal_handler(gpointer user_data)
 
 int main(int argc, char **argv)
 {
-    g_print("Hello!\n");
+    parse_options(argc, argv);
 
     g_autoptr(GMainLoop) _loop = loop = g_main_loop_new(NULL, FALSE);
 
