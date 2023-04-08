eb26988469b190508adf8e44cea6da4009153348
Martin Domig <martin@domig.net>
2023-04-08 14:42:22 +0200

    glib: main loop + signal handling

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <glib.h>
+#include <glib-unix.h>
+
+static GMainLoop *loop = NULL;
+
+static gboolean signal_handler(gpointer user_data)
+{
+    g_print("Signal received");
+    g_main_loop_quit(loop);
+    return G_SOURCE_REMOVE;
+}
 
 int main(int argc, char **argv)
 {
     g_print("Hello!\n");
+
+    g_autoptr(GMainLoop) _loop = loop = g_main_loop_new(NULL, FALSE);
+
+    g_unix_signal_add(SIGTERM, signal_handler, NULL);
+
+    g_print("Running main loop");
+    g_main_loop_run(_loop);
+
+    g_print("Terminating");
     return EXIT_SUCCESS;
 }
